33dccf5c23afa3563ae8b80961702ed6101f944c
Ramanathan Perumal PhD <16385390+ramamet@users.noreply.github.com>
2023-05-15 11:39:22 +0530

    delete docker files

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-echo "Thanks for using this GH action!"
-input_file=$1
-output_format=$2
-
-ln -s /render.R .
-
-Rscript render.R $input_file $output_format
--- render.R
+++
@@ -1,3 +0,0 @@
-args = commandArgs(trailingOnly = TRUE)
-library(rmarkdown)
-render(args[1], output_format = args[2])
